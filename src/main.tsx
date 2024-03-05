6f15265a4427877b5ab1ea8cf7b671d27ba4c114
Andrew Caetano <andrewr_caetano@hotmail.com>
2024-03-05 19:33:06 -0300

    Fix Provider - Agora Vai

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
 import App from './App';
 import rootReducer from './redux/reducers'; // seu arquivo de reducers
 
