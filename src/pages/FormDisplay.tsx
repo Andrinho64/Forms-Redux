23bd41f34f04175c92b627c27368863d8e8a0577
Andrew Caetano <andrewr_caetano@hotmail.com>
2024-03-05 19:18:04 -0300

    Req 4

--- src/pages/FormDisplay.tsx
+++ src/pages/FormDisplay.tsx
@@ -1,44 +1,42 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { PersonalType, ProfessionalType } from '../Type/type';
+
 function FormDisplay() {
-  // Recupere as informações do seu estado criado no Redux
+  const professionalData = useSelector(
+    (state: ProfessionalType) => state.professionalData,
+  );
+  const personalData = useSelector(
+    (state: PersonalType) => state.personalData,
+  );
+
+  const {
+    resume,
+    role,
+    description,
+  } = professionalData;
+
+  const {
+    address,
+    city,
+    cpf,
+    email,
+    name,
+    uf,
+  } = personalData;
+
   return (
     <section>
       <h1>Dados Enviados</h1>
-      <div>
-        Nome:
-        {/* { name } */}
-      </div>
-      <div>
-        Email:
-        {/* { email } */}
-      </div>
-      <div>
-        CPF:
-        {/* { cpf } */}
-      </div>
-      <div>
-        Endereço:
-        {/* { address } */}
-      </div>
-      <div>
-        Cidade:
-        {/* { city } */}
-      </div>
-      <div>
-        Estado:
-        {/* { uf } */}
-      </div>
-      <div>
-        Currículo:
-        {/* { resume } */}
-      </div>
-      <div>
-        Cargo:
-        {/* { role } */}
-      </div>
-      <div>
-        Descrição do cargo:
-        {/* { description } */}
-      </div>
+      <div>{`Nome: ${name}`}</div>
+      <div>{`Email: ${email}`}</div>
+      <div>{`CPF: ${cpf}`}</div>
+      <div>{`Endereço: ${address}`}</div>
+      <div>{`Cidade: ${city}`}</div>
+      <div>{`Estado: ${uf}`}</div>
+      <div>{`Currículo: ${resume}`}</div>
+      <div>{`Cargo: ${role}`}</div>
+      <div>{`Descrição do cargo: ${description}`}</div>
     </section>
   );
 }
