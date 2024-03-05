d51a2d280861358a1ecbb3cbedaa27fbba0771a7
Andrew Caetano <andrewr_caetano@hotmail.com>
2024-03-05 19:31:17 -0300

    Fix Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+import rootReducer from './redux/reducers'; // seu arquivo de reducers
+
+const store = createStore(rootReducer);
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
   .render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={ store }>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
   );
